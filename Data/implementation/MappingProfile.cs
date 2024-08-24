540c52a4ceeb857a65411024903c2649a3ff1256
Rafael Vasconcelos <youkko@gmail.com>
2024-08-24 01:34:56 -0300

    Removed comments form MappingProfile

--- Data/implementation/MappingProfile.cs
+++ Data/implementation/MappingProfile.cs
@@ -12,15 +12,10 @@ namespace MotorcycleRental.Data
             CreateMap<DB.DeliveryPerson, DTO.DeliveryPerson>().PreserveReferences().ReverseMap();
             CreateMap<DB.Vehicle, DTO.Vehicle>().PreserveReferences().ReverseMap();
             CreateMap<DB.Vehicle, DTO.VehicleSimplified>().PreserveReferences().ReverseMap();
-            //CreateMap<DB.Order, DTO.Order>().PreserveReferences().ReverseMap();
-            //CreateMap<DB.OrderItem, DTO.OrderItem>().PreserveReferences().ReverseMap();
             CreateMap<DB.Rental, DTO.Rental>().PreserveReferences().ReverseMap();
             CreateMap<DB.RentalPlan, DTO.RentalPlan>().PreserveReferences().ReverseMap();
             CreateMap<DB.CNHType, DTO.CNHType>().PreserveReferences().ReverseMap();
             CreateMap<DB.Delivery, DTO.Delivery>().PreserveReferences().ReverseMap();
-            //CreateMap<DB.Notification, DTO.Notification>().PreserveReferences().ReverseMap();
-
-            // Configure Rental to RentalInfo mapping
             CreateMap<DB.Rental, DTO.RentalInfo>()
                 .ForMember(dest => dest.Vehicle, opt => opt.MapFrom(src => src.Vehicle))
                 .ForMember(dest => dest.Plan, opt => opt.MapFrom(src => src.RentalPlan))
